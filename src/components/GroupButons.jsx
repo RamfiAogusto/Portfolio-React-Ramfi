0cffe27884a0a46a50c2a1bda1cfabc5e8f94705
Ramfiaogusto <ramfiaogusto@gmail.com>
2024-11-17 15:27:07 -0400

    sin insta

--- src/components/GroupButons.jsx
+++ src/components/GroupButons.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import iconInsta from "../assets/icons/instagram_dark.svg";
+/* import iconInsta from "../assets/icons/instagram_dark.svg"; */
 import iconLinkedin from "../assets/icons/linkedin.svg";
 import iconGithub from "../assets/icons/github.svg";
 import Cv from "../assets/archivos/cv.pdf";
@@ -23,17 +23,7 @@ function GroupButons() {
                     Descargar CV
                 </a>
                 <div className="flex gap-2">
-                    <a
-                        href="https://www.instagram.com/ramfi_aog/"
-                        target="_blank"
-                        className="p-2 inline-block rounded-[10px] bg-transparent border-solid border-[2px] border-[var(--primary)] w-11 h-11 filter-[var(--sombra)]"
-                    >
-                        <img
-                            src={iconInsta}
-                            alt="icono instagram"
-                            className="w-full"
-                        />
-                    </a>
+                    
                     <a
                         href="https://www.linkedin.com/in/ramfi-aogusto-de-la-cruz-geraldino-aa64b5272"
                         target="_blank"
